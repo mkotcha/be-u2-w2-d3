c2a440452ae0dd62653ea3e4efcefc2e0a302243
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-08 10:15:10 +0100

    firt commit

---
+++ src/main/java/org/emmek/beu2w2d3/EpicodeApplication.java
@@ -0,0 +1,13 @@
+package org.emmek.beu2w2d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/emmek/beu2w2d3/EpicodeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emmek.beu2w2d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
